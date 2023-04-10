26a106f1e672c9da016a687986ef4c17841938a0
kizahasi <21971954+kizahasi@users.noreply.github.com>
2023-04-10 17:55:15 +0900

    refactor: use # for OperationBuilder member names

--- src/internal/operationBuilder/operationBuilder.ts
+++ src/internal/operationBuilder/operationBuilder.ts
@@ -8,76 +8,76 @@ import { OperationElement } from './operationElement';
 import { OperationUnit } from './operationUnit';
 
 export class OperationBuilder<TInsert, TDelete> {
-    public constructor(
-        private factory: OperationBuilderFactory<TInsert, TDelete>,
-        source?: Operation<TInsert, TDelete>
-    ) {
+    readonly #factory: OperationBuilderFactory<TInsert, TDelete>;
+
+    constructor(factory: OperationBuilderFactory<TInsert, TDelete>, source?: Operation<TInsert, TDelete>) {
+        this.#factory = factory;
         if (source == null) {
             return;
         }
-        this.headEdit = source.headEdit ?? null;
-        this.body = Array.from(source.body);
-        this.tailRetain = source.tailRetain ?? 0;
+        this.#headEdit = source.headEdit ?? null;
+        this.#body = Array.from(source.body);
+        this.#tailRetain = source.tailRetain ?? 0;
     }
 
-    private headEdit: EditElement<TInsert, TDelete> | null = null;
-    private readonly body: OperationElement<TInsert, TDelete>[] = [];
-    private tailRetain: PositiveInt | 0 = 0;
+    #headEdit: EditElement<TInsert, TDelete> | null = null;
+    readonly #body: OperationElement<TInsert, TDelete>[] = [];
+    #tailRetain: PositiveInt | 0 = 0;
 
-    public retain(count: PositiveInt): void {
-        if (this.tailRetain === 0) {
-            this.tailRetain = count;
+    retain(count: PositiveInt): void {
+        if (this.#tailRetain === 0) {
+            this.#tailRetain = count;
             return;
         }
-        this.tailRetain = PositiveInt.add(this.tailRetain, count);
+        this.#tailRetain = PositiveInt.add(this.#tailRetain, count);
     }
 
-    public edit(edit: EditElement<TInsert, TDelete>) {
-        if (this.tailRetain !== 0) {
-            this.body.push({
-                firstRetain: this.tailRetain,
+    edit(edit: EditElement<TInsert, TDelete>) {
+        if (this.#tailRetain !== 0) {
+            this.#body.push({
+                firstRetain: this.#tailRetain,
                 secondEdit: edit,
             });
-            this.tailRetain = 0;
+            this.#tailRetain = 0;
             return;
         }
 
-        if (this.body.length !== 0) {
+        if (this.#body.length !== 0) {
             // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-            const last = this.body[this.body.length - 1]!;
-            this.body[this.body.length - 1] = {
+            const last = this.#body[this.#body.length - 1]!;
+            this.#body[this.#body.length - 1] = {
                 ...last,
                 secondEdit: mergeEditElement(
                     last.secondEdit,
                     edit,
-                    (x, y) => this.factory.concatInsert(x, y),
-                    (x, y) => this.factory.concatDelete(x, y)
+                    (x, y) => this.#factory.concatInsert(x, y),
+                    (x, y) => this.#factory.concatDelete(x, y)
                 ),
             };
             return;
         }
 
-        if (this.headEdit == null) {
-            this.headEdit = edit;
+        if (this.#headEdit == null) {
+            this.#headEdit = edit;
             return;
         }
-        this.headEdit = mergeEditElement(
-            this.headEdit,
+        this.#headEdit = mergeEditElement(
+            this.#headEdit,
             edit,
-            (x, y) => this.factory.concatInsert(x, y),
-            (x, y) => this.factory.concatDelete(x, y)
+            (x, y) => this.#factory.concatInsert(x, y),
+            (x, y) => this.#factory.concatDelete(x, y)
         );
     }
 
-    public insert(insert: TInsert): void {
+    insert(insert: TInsert): void {
         this.edit({ type: insert$, insert });
     }
 
-    public delete(del: TDelete): void {
+    delete(del: TDelete): void {
         this.edit({ type: delete$, delete: del });
     }
 
-    public onArrayElement(arrayElement: OperationArrayElement<TInsert, TDelete>) {
+    onArrayElement(arrayElement: OperationArrayElement<TInsert, TDelete>) {
         switch (arrayElement.type) {
             case retain:
                 this.retain(arrayElement.retain);
@@ -87,7 +87,7 @@ export class OperationBuilder<TInsert, TDelete> {
         }
     }
 
-    public onUnit(unit: OperationUnit<TInsert, TDelete>) {
+    onUnit(unit: OperationUnit<TInsert, TDelete>) {
         if (unit.type === retain) {
             this.retain(unit.retain);
             return;
@@ -95,15 +95,15 @@ export class OperationBuilder<TInsert, TDelete> {
         this.edit(unit);
     }
 
-    public build(): Operation<TInsert, TDelete> {
+    build(): Operation<TInsert, TDelete> {
         return {
-            headEdit: this.headEdit ?? undefined,
-            body: Array.from(this.body),
-            tailRetain: this.tailRetain === 0 ? undefined : this.tailRetain,
+            headEdit: this.#headEdit ?? undefined,
+            body: Array.from(this.#body),
+            tailRetain: this.#tailRetain === 0 ? undefined : this.#tailRetain,
         };
     }
 
-    public *toIterable(): IterableIterator<OperationArrayElement<TInsert, TDelete>> {
+    *toIterable(): IterableIterator<OperationArrayElement<TInsert, TDelete>> {
         const operation = this.build();
         if (operation.headEdit != null) {
             yield { type: edit, edit: operation.headEdit };
@@ -117,7 +117,7 @@ export class OperationBuilder<TInsert, TDelete> {
         }
     }
 
-    public *toUnits(): IterableIterator<OperationUnit<TInsert, TDelete>> {
+    *toUnits(): IterableIterator<OperationUnit<TInsert, TDelete>> {
         for (const elem of this.toIterable()) {
             if (elem.type === retain) {
                 yield { type: retain, retain: elem.retain };
